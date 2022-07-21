afdfdf434ca8b03a9b18ba33dd45cc3a6037e03a
Mark Corpuz <social@markcorpuz.com>
2022-07-21 10:12:21 -0700

    URC - Unique main and grid feature styles for URC

--- templates/views/urc-gridfeature.php
+++ templates/views/urc-gridfeature.php
@@ -7,13 +7,12 @@ $pid = $mfunc->setup_array_validation( 'pid', $bars );
 
 // class
 $cs = array(
-	'manual_class'		=> 'item-entry urc-mainfeature',
+	'manual_class'		=> 'item-entry urc-gridfeature',
 	'item_class' 		=> $mfunc->setup_array_validation( 'wrap_sel', $bars ),
 	'block_class'		=> $mfunc->setup_array_validation( 'block_class', $bars ),
 );
 $css = $mfunc->setup_pull_combine_classes( $cs );
 $classes = !empty( $css ) ? ' class="'.$css.'"' : '';
-// var_dump( $mfunc->setup_array_validation( 'wrap_sel', $bars ) );
 
 // styles
 $ss = array(
@@ -27,26 +26,20 @@ $inline_style = !empty( $stayls ) ? ' style="'.$stayls.'"' : '';
  * CONTENT | START
  */
 
-// featured media/image
-$feat_img = get_the_post_thumbnail_url( $pid, "large" );
-
 // WRAP | OPEN
 echo '<div'.$classes.$inline_style.'>';
 
-echo '<div class="items-image">';
-
-	echo '<a class="item-thumbnail" href="'.get_the_permalink( $pid ).'" style="background-image:url('.$feat_img.');"></a>';
+	// featured media/image
+	$feat_img = get_the_post_thumbnail_url( $pid, "medium_large" );
+	if( !empty( $feat_img ) && !empty( $mfunc->setup_field_control_validation( 'featured_media', $mfunc->setup_array_validation( "field_control", $bars ) ) ) ) {
+		echo '<div class="item-thumbnail space-small"><a href="'.get_the_permalink( $pid ).'"><img src="'.$feat_img.'" border="0" /></a></div>';
+	}
 
 	// wp-title
 	$wp_title = get_the_title( $pid );
 	if( !empty( $wp_title ) && !empty( $mfunc->setup_field_control_validation( 'title', $mfunc->setup_array_validation( "field_control", $bars ) ) ) ) {
-		//echo '<div class="item-title"><a href="'.get_the_permalink( $pid ).'">'.$wp_title.'</a></div>';
-		echo '<a class="item-title" href="'.get_the_permalink( $pid ).'">'.$wp_title.'</a>';	
+		echo '<div class="item-title textsize-medium textweight-bold"><a href="'.get_the_permalink( $pid ).'">'.$wp_title.'</a></div>';
 	}
-
-echo '</div>';
-
-echo '<div class="items-info">';
 	
 	// wp-content
 	$wp_content = $mfunc->setup_pull_apply_filters_to_content( $pid );
@@ -57,23 +50,22 @@ echo '<div class="items-info">';
 	// wp-excerpt
 	$wp_excerpt = get_the_excerpt( $pid );
 	if( !empty( $wp_excerpt ) && !empty( $mfunc->setup_field_control_validation( 'excerpt', $mfunc->setup_array_validation( "field_control", $bars ) ) ) ) {
-		echo '<div class="item-excerpt">'.$wp_excerpt.'</div>';
+		echo '<div class="item-excerpt textsize-small">'.$wp_excerpt.'</div>';
 	}
 
 	// date modified
+	/*
 	if( !empty( $mfunc->setup_field_control_validation( 'modified', $mfunc->setup_array_validation( "field_control", $bars ) ) ) ) {
-		echo '<div class="item-modified">'.get_the_modified_date( "F j, Y, g:i a", $pid ).'</div>';
+		echo '<div class="item-modified"><b>MODIFIED:</b> '.get_the_modified_date( "F j, Y, g:i a", $pid ).'</div>';
 	}
+	*/
 	
 	// date published
-	if( !empty( $mfunc->setup_field_control_validation( 'date_published', $mfunc->setup_array_validation( "field_control", $bars ) ) ) ) {
-		echo '<div class="item-published">'.get_the_date( "F j, Y, g:i a", $pid ).'</div>';
-	}
-
-	//echo '<div class="item-cta"><a href="'.get_the_permalink( $pid ).'">MORE INFO</a></div>';
 	/*
-	echo '<div class="item-cta">READ ARTICLE</div>';
-	 */
+	if( !empty( $mfunc->setup_field_control_validation( 'date_published', $mfunc->setup_array_validation( "field_control", $bars ) ) ) ) {
+		echo '<div class="item-published"><b>PUBLISHED:</b> '.get_the_date( "F j, Y, g:i a", $pid ).'</div>';
+	}
+	*/
 
 	// ACF | Title
 	/*$acf_title = $mfunc->setup_array_validation( "title", $bars );
@@ -115,11 +107,9 @@ echo '<div class="items-info">';
 	// SOURCE
 	$e_source = $mfunc->setup_array_validation( "sources", $bars );
 	if( !empty( $e_source ) && $e_source === TRUE ) {
-		/*$link = '<a href="'.get_the_permalink( $pid ).'">'.get_the_title( $pid ).'</a>';
-		echo '<div class="item-cta">'.$link.'</div>';*/
-		echo '<div class="item-cta">'.get_the_title( $pid ).'</div>';
+		$link = '<a href="'.get_the_permalink( $pid ).'">'.get_the_title( $pid ).'</a>';
+		echo '<div class="item-entry-source"><b>SOURCE:</b> '.$link.'</div>';
 	}
 
-echo '</div>';
 // WRAP | CLOSE
 echo '</div>';
